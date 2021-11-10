1c5395f4892b59630c944b00faab70f22d9d4d58
wertzuk <pascalb95@gmx.de>
2021-11-10 23:43:32 +0100

    finished project

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,4 +1,24 @@
-const BASE_URL = "https://restcountries.com/v2/name";
+const theme = document.querySelector(".theme");
+const icon = document.querySelector(".theme i");
+theme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
+  icon.classList.toggle("fa-light");
+  // icon.classList.toggle("fa-solid");
+});
+
+function shuffle(a) {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+const BASE_URL = "https://restcountries.com/v2";
 
 const countries = [
   "germany",
@@ -11,42 +31,66 @@ const countries = [
   "algeria",
 ];
 
-async function getData(country) {
-  const response = await fetch(`${BASE_URL}/${country}`);
+async function getData(search, type) {
+  const response = await fetch(`${BASE_URL}/${type}/${search}`);
   const json = await response.json();
-  return json[0];
+  return json;
 }
+let timer;
 
 const app = new Vue({
   el: "#app",
   created() {
     countries.forEach(async (country) => {
-      const data = await getData(country);
-      this.countries.push(data);
+      const data = await getData(country, "name");
+      this.countries.push(data[0]);
     });
   },
   data() {
     return {
+      darkTheme: true,
       countries: [],
+      countryDetails: null,
       searchTerm: "",
+      matchingCountries: [],
       detailActive: false,
     };
   },
   methods: {
-    getDetails(e) {
-      if (e.key === "Enter") {
-        this.detailActive = true;
+    async processInput() {
+      this.matchingCountries = [];
+      clearTimeout(timer);
+      if (this.searchTerm.length > 2) {
+        timer = setTimeout(async () => {
+          const currentData = await getData(this.searchTerm, "name");
+          if (currentData.status !== 404) {
+            currentData.forEach((data) => {
+              this.matchingCountries.push(data.name);
+            });
+          }
+        }, 350);
       }
     },
-  },
-  computed: {
-    async updatedData() {
-      if (this.searchTerm.length > 1) {
-        const country = await getData(this.searchTerm);
-        if (country) {
-          console.log(country);
-        }
+
+    async showDetails(countryName) {
+      this.matchingCountries = [];
+      const data = await getData(countryName, "name");
+      const countryData = data[0];
+      if (countryData) {
+        this.detailActive = true;
+        this.countryDetails = countryData;
+
+        this.searchTerm = "";
       }
+    },
+    goBack() {
+      this.detailActive = false;
+      this.countryDetails = null;
+    },
+    async filterCountries(region) {
+      this.countries = [];
+      const data = await getData(region, "region");
+      this.countries = data;
     },
   },
 });
